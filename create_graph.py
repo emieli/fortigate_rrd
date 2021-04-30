f6fac5e7843dff94368512f78969f9c7fb3a5b29
emil eliasson <emil@fortigate-rrd.golle.org>
2021-04-30 20:58:44 +0200

    skript gör vad det ska

--- create_graph.py
+++ create_graph.py
@@ -2,28 +2,38 @@ import os
 import rrdtool
 import datetime
 import time
+import json
 
-data_folder = f"{os.path.dirname(os.path.realpath(__file__))}/rrd_data"
-images_folder = f"{os.path.dirname(os.path.realpath(__file__))}/images"
+data_folder = f"{os.path.dirname(os.path.realpath(__file__))}/rrd"
+graphs_folder = f"{os.path.dirname(os.path.realpath(__file__))}/graphs"
+if not os.path.exists(graphs_folder):
+    os.makedirs(graphs_folder)
+
+end_time = datetime.datetime.now() - datetime.timedelta()
+end_time = int(time.mktime(end_time.timetuple()))
+start_time = datetime.datetime.now() - datetime.timedelta(minutes = 40)
+start_time = int(time.mktime(start_time.timetuple()))
+
 data_files = os.listdir(data_folder)
-
-end_time = datetime.datetime.now()
-start_time = datetime.datetime.now() - datetime.timedelta(minutes = 120)
-
 for data_file in data_files:
-    
+
     input_file = f"{data_folder}/{data_file}"
     image_file = data_file.replace(".rrd", ".png")
 
     ''' Create the .png file and place it in the /static/ folder. '''
-    rrdtool.graph(f"{images_folder}/{image_file}", 
-        "--start", f"{int(time.mktime(start_time.timetuple()))}",
-        "--end", f"{int(time.mktime(end_time.timetuple()))}",
+    rrdtool.graph(f"{graphs_folder}/{image_file}", 
+        "--start", f"{start_time}",
+        "--end", f"{end_time}",
         f"--title={data_file.replace('.rrd', '')} Channel Utilization",
-        "--height=150",
-        "--width=470",
+        "--height=300",
+        "--width=900",
         "--upper-limit=100",
-        f"DEF:ch-util-2ghz={input_file}:ch-util-2ghz:MAX",
-        "LINE:ch-util-2ghz#c334eb:2.4 ghz %",
-        f"DEF:ch-util-5ghz={input_file}:ch-util-5ghz:MAX",
-        "LINE:ch-util-5ghz#f59c42:5 ghz %")
+        "--lower-limit=0",
+        f"DEF:ch-util-2ghz-max={input_file}:ch-util-2ghz:MAX",
+        "LINE:ch-util-2ghz-max#A22615:2.4 ghz max%:dashes=4",
+        f"DEF:ch-util-2ghz-avg={input_file}:ch-util-2ghz:AVERAGE",
+        "LINE2:ch-util-2ghz-avg#F26C52:2.4 ghz avg%",
+        f"DEF:ch-util-5ghz-max={input_file}:ch-util-5ghz:MAX",
+        "LINE:ch-util-5ghz-max#003046:5 ghz max%:dashes=4",
+        f"DEF:ch-util-5ghz-avg={input_file}:ch-util-5ghz:AVERAGE",
+        "LINE2:ch-util-5ghz-avg#006EB9:5 ghz avg%")
--- script.py
+++ script.py
@@ -20,7 +20,9 @@ import getpass
 username = getpass.getuser()
 password = getpass.getpass(f"Enter Fortigate password ({username}): ")
 
-data_folder = f"{os.path.dirname(os.path.realpath(__file__))}/rrd_data"
+data_folder = f"{os.path.dirname(os.path.realpath(__file__))}/rrd"
+if not os.path.exists(data_folder):
+    os.makedirs(data_folder)
 
 ''' Connect to Fortigate via SSH '''
 ssh = paramiko.SSHClient()
@@ -31,31 +33,17 @@ while True:
     stdin, stdout, stderr = ssh.exec_command("diagnose wireless-controller wlac -c wtp")
     stdin.close()
 
-    ch_util = {
-        '2.4ghz': None,
-        '5ghz': None
-    }
+    ch_util = {}
     for line in stdout.readlines():
         line = line.strip()
 
         ''' Next AP, clear variable '''
         if "-------------------------------WTP" in line:
-            ch_util = {
-                '2.4ghz': None,
-                '5ghz': None
-            }
+            ch_util = {}
         
         ''' Fetch AP name, create RRD file '''
         if "name             : " in line:
             ap_name = line.split(": ")[1]
-            filename = f"{data_folder}/{ap_name}.rrd"
-            if not os.path.isfile(filename):
-                rrdtool.create(filename, 
-                    "--start", "now",
-                    "--step", "15",
-                    "DS:ch-util-2ghz:GAUGE:60:0:100", # wait up to 60 seconds for input, expect value between 0 and 100
-                    "DS:ch-util-5ghz:GAUGE:60:0:100",
-                    "RRA:MAX:0.5:4:525600") # 1 year history, in steps of 1 minute. Since data is polled 4 times per minute, the highest value is saved.
 
         ''' Fetch current radio '''
         if line == "Radio 1            : AP":
@@ -65,13 +53,29 @@ while True:
 
         ''' Fetch current channel utilization '''
         if "oper chutil data : " in line:
-            ''' Actual output: 'oper chutil data : 27,44,36,36,30, 33,29,30,29,20, 24,23,20,22,23 ->newer'
-                We do some ugly string splitting to retrieve the first value, in this case 27. '''
-            current_ch_util = line.split(": ")[1].split(",")[0]
-            ch_util[ap_radio] = current_ch_util
+            try:
+                ''' Actual output: 'oper chutil data : 27,44,36,36,30, 33,29,30,29,20, 24,23,20,22,23 ->newer' 
+                    We do some ugly string splitting to retrieve the first value, in this case 27. '''
+                current_ch_util = int(line.split(": ")[1].split(",")[0])
+            except:
+                print("Unknown channel utilization value:")
+                print(line)
+                pass
+            else:
+                ch_util[ap_radio] = current_ch_util
 
             ''' If all data has been gathered, write to RRD file '''
-            if (ch_util['2.4ghz'] != None) and (ch_util['5ghz'] != None):
+            if '2.4ghz' in ch_util and '5ghz' in ch_util:
+                filename = f"{data_folder}/{ap_name}.rrd"
+                if not os.path.isfile(filename):
+                    rrdtool.create(filename, 
+                        "--start", "now",
+                        "--step", "15",
+                        "DS:ch-util-2ghz:GAUGE:60:0:100", # wait up to 60 seconds for input, expect value between 0 and 100
+                        "DS:ch-util-5ghz:GAUGE:60:0:100",
+                        "RRA:MAX:0.25:4:525600",
+                        "RRA:AVERAGE:0.25:4:525600")
+                
                 rrdtool.update(filename, f"N:{ch_util['2.4ghz']}:{ch_util['5ghz']}")
                 print(f"N:{ch_util['2.4ghz']}:{ch_util['5ghz']}")
 
